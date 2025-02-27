3a79f93bd573dcbdb9a70e39812c29843c810f3b
pmmpChris <54825739+PDevChris@users.noreply.github.com>
2025-02-26 21:23:24 -0500

    Update SpecterPlayer.php

--- src/specter/network/SpecterPlayer.php
+++ src/specter/network/SpecterPlayer.php
@@ -9,19 +9,51 @@ use pocketmine\Player;
 class SpecterPlayer extends Player {
     public $spec_needRespawn = false;
     private $forceMovement;
-    public function __construct(SourceInterface $interface, $ip, $port){
+
+    /**
+     * SpecterPlayer constructor.
+     *
+     * @param SourceInterface $interface
+     * @param string $ip
+     * @param int $port
+     */
+    public function __construct(SourceInterface $interface, string $ip, int $port){
         parent::__construct($interface, $ip, $port);
     }
+
     /**
+     * Gets the force movement of the player.
+     *
      * @return Vector3
      */
-    public function getForceMovement(){
+    public function getForceMovement(): ?Vector3 {
         return $this->forceMovement;
     }
-	/**
-	 * @return PlayerNetworkSessionAdapter
-	 */
-    public function getSessionAdapter() {
-    	return $this->sessionAdapter;
+
+    /**
+     * Gets the session adapter for the player.
+     *
+     * @return PlayerNetworkSessionAdapter|null
+     */
+    public function getSessionAdapter(): ?PlayerNetworkSessionAdapter {
+        return $this->sessionAdapter ?? null;
+    }
+
+    /**
+     * Set force movement for the player.
+     *
+     * @param Vector3 $forceMovement
+     */
+    public function setForceMovement(Vector3 $forceMovement): void {
+        $this->forceMovement = $forceMovement;
+    }
+
+    /**
+     * Sets whether the player needs to respawn.
+     *
+     * @param bool $spec_needRespawn
+     */
+    public function setNeedRespawn(bool $spec_needRespawn): void {
+        $this->spec_needRespawn = $spec_needRespawn;
     }
 }
